5da2cb90ced50997dad6a7f0c831f6d2bf7a7fae
SohamDutta <94747591+Sohamdutt@users.noreply.github.com>
2024-10-30 15:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from routes import initialize_routes
-import config
+from create_app import create_app
 
-app = Flask(__name__)
-app.config.from_object(config)
-
-# Initialize Database
-db = SQLAlchemy(app)
-
-# Initialize Routes
-initialize_routes(app)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
